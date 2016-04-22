b80876425574298085c2ce6985177a18805c9e77
Stefan Heimberg, PF52 <stefan.heimberg@postfinance.ch>
2016-04-22 13:31:17 +0200

    code2schema

---
+++ code2schema/src/main/java/ch/stefanheimberg/examples/jaxb/Person.java
@@ -0,0 +1,47 @@
+package ch.stefanheimberg.examples.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {"name", "adresse"})
+@XmlRootElement(namespace="http://stefanheimberg.ch/examples/jaxb", name = "Person")
+public class Person {
+    
+    @XmlElement(required = true)
+    protected String name;
+
+    protected String adresse;
+
+    @XmlAttribute(required = true)
+    protected long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+}
